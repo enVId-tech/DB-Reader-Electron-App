aef8d39c4a3f0c213efdd3977f93860d7be4b797
Erick Tran <thelittlebotengineer@gmail.com>
2024-12-25 14:48:32 -0800

    feat(src): work on app ui
    Worked on Eletron app UI as well as configuration inside of app.tsx and the styling inside of
    main.scss

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import {createRoot, Root} from 'react-dom/client';
 import Main from "./pages/Main";
+import React from "react";
 
 const root: Root = createRoot(document.body);
 root.render(<Main/>);
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,10 +3,15 @@ import './main.scss';
 
 const Main = (): React.JSX.Element => {
     const [pageState, setPageState] = React.useState<string>("");
+    const [dbsToLoad, setDbsToLoad] = React.useState<string[]>([]);
 
     React.useEffect(() => {
         setPageState("Main");
     }, []);
+
+    const appendDb = (db: string): void => {
+        setDbsToLoad([...dbsToLoad, db]);
+    }
 
     return (
         <>
@@ -15,7 +20,17 @@ const Main = (): React.JSX.Element => {
                     pageState === "Main" ? (
                         <div className={"main-content"}>
                             <div className={"left-nav"}>
-
+                                <h1 className={"left-nav-title"}>Load Databases</h1>
+                                {
+                                    dbsToLoad.map((db: string, index: number) => {
+                                        return (
+                                            <div key={index} className={"left-nav-item"}>
+                                                <p className={"left-nav-item-text"}>{db}</p>
+                                            </div>
+                                        );
+                                    })
+                                }
+                                <button onClick={() => appendDb("Test")}>Append Database</button>
                             </div>
                             <div className={"right-content"}>
                                 <h1 className={"main-content-title"}>Main Page</h1>
